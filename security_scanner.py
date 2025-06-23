f60787d22b23daf2af81f974844c553b4391f8cb
Lain <bluebary@gmail.com>
2025-06-23 16:42:13 +0900

    .gitignore 및 README.md 업데이트 및 보안 스캐너의 웹드라이버 설정 개선

--- security_scanner.py
+++ security_scanner.py
@@ -50,6 +50,8 @@ class SecurityScanner:
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--allow-insecure-localhost")
         
         # 웹드라이버 설정
         try:
